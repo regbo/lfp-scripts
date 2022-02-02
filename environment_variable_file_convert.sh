67eae7f0cd31b94701bc150dfe1c62c888fae18b
regbo <reggie.pierce@gmail.com>
2022-02-02 10:26:54 -0500

    Update environment_variable_file_convert.sh

--- environment_variable_file_convert.sh
+++ environment_variable_file_convert.sh
@@ -1,19 +1,22 @@
 env | while IFS= read -r line; do
   name=${line%%=*}
-  if [[ "$name" != *_FILE ]]; then
+  if( ! echo "$name" | grep -i "_FILE$" > /dev/null ); then
     continue
   fi
   value=${line#*=}
-  if [[ -z "$value" ]]; then
+  if [ -z "$value" ]; then
+    continue
+  fi
+  if [ ! -e "$value" ]; then
     continue
   fi
   nameNoFile=$(echo ${name%_FILE*})
   valueNoFile="${!nameNoFile}"
-  if [[ ! -z "$valueNoFile" ]]; then
+  if [ ! -z "$valueNoFile" ]; then
     continue
   fi
   valueFile=$(cat "$value")
-  if [[ -z "$valueFile" ]]; then
+  if [ -z "$valueFile" ]; then
     continue
   fi
   export "$nameNoFile"="$valueFile"
